e23a175009d961e42848f894e34266444470d478
Andrej Petras <andrej@lorislab.org>
2018-01-11 22:52:31 +0100

    Update for the release

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export VERSION=0.7.1-SNAPSHOT
+export RELEASE=$1
+
+git clean -f && git reset --hard origin/master
+
+mvn -DtagNameFormat=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${VERSION} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare release:perform -B 
+
+git push origin $RELEASE
+
